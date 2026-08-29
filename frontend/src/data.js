2fe592badb5cdccdb921a1ebb11901dd0ae1850f
rayhanrinzan <rayhanrinzan@gmail.com>
2026-08-29 12:27:22 -0400

    formatted featured products layout

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -4,7 +4,7 @@ const data = {
       name: 'Product 1',
       slug: 'product-1',
       category: 'Category 1',
-      image: '/downloads/product_1.jpg',
+      image: '/images/product_1.jpg',
       price: '15',
       countInStock: 10,
       rating: 4.5,
@@ -13,7 +13,7 @@ const data = {
     },
     {
       name: 'Product 1',
-      slug: 'product-1',
+      slug: 'product-2',
       category: 'Category 1',
       image: '/images/product_1.jpg',
       price: '15',
@@ -24,7 +24,7 @@ const data = {
     },
     {
       name: 'Product 1',
-      slug: 'product-1',
+      slug: 'product-3',
       category: 'Category 1',
       image: '/images/product_1.jpg',
       price: '15',
